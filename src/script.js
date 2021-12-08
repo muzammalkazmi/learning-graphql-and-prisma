72f8a05729fbb2653ee18b6003fd19a9a766690f
muzammal kazmi <muzammalkazmi@gmail.com>
2021-12-08 11:36:23 +0500

    add sqlite and created table

--- src/script.js
+++ src/script.js
@@ -1,13 +1,19 @@
 const { PrismaClient } = require('@prisma/client')
-const client = new PrismaClient()
+const prisma = new PrismaClient()
 async function main() {
+    const newLink = await prisma.link.create({
+        data: {
+            description: 'Fullstack tutorial for GraphQL',
+            url: 'www.howtographql.com',
+        }
+    })
     const allLinks = await prisma.link.findMany()
     console.log(allLinks)
 }
 main()
-    .catch(e => {
+    .catch((e) => {
         throw e
     })
     .finally(async() => {
-        await prisma.disconnect()
+        await prisma.$disconnect()
     })
